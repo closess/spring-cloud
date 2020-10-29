8d16ebe693b424e1bdc5fdfcf0cb2ec3d83e4629
weimingwen <2662460899@qq.com>
2020-10-29 21:48:31 +0800

    服务之间的调用之基础环境的搭建

---
+++ codes/users9999/src/main/java/com/Users9999Application.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient//加不加都行
+public class Users9999Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Users9999Application.class, args);
+    }
+
+}
