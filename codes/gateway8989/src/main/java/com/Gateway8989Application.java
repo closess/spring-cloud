11ab1727d8a39fa502e367bc90b6ce4997976fad
changan <changanwei266@gmail.com>
2020-11-10 10:30:20 +0800

    gateway网关路由转发

---
+++ codes/gateway8989/src/main/java/com/Gateway8989Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gateway8989Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gateway8989Application.class, args);
+	}
+
+}
