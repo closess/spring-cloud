1db96445a8648676baeb70e31e70ced47839e139
changan <changanwei266@gmail.com>
2020-11-10 20:10:08 +0800

    路由转发之断言&过滤器

--- codes/gateway8989/src/test/java/com/Gateway8989ApplicationTests.java
+++ codes/gateway8989/src/test/java/com/Gateway8989ApplicationTests.java
@@ -3,11 +3,20 @@ package com;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.ZonedDateTime;
+
+//@SpringBootTest
 class Gateway8989ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//		ZonedDateTime now = ZonedDateTime.now();
+//		System.out.println(now);
+//	}
+      public static void main(String[] args) {
+			ZonedDateTime now = ZonedDateTime.now();
+			System.out.println(now);//2020-11-10T16:27:55.608+08:00[Asia/Shanghai]
+
+}
 
 }
