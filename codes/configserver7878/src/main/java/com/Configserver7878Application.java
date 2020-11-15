5a59b3900d9348c7f338ab65fece713a19b5034e
changan <changanwei266@gmail.com>
2020-11-15 18:00:31 +0800

    webhooks实现配置自动刷新

--- codes/configserver7878/src/main/java/com/Configserver7878Application.java
+++ codes/configserver7878/src/main/java/com/Configserver7878Application.java
@@ -1,9 +1,12 @@
 package com;
 
+import com.filters.UrlFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@ServletComponentScan(basePackages = "com.filters")
 @SpringBootApplication
 @EnableConfigServer//开启统一配置中心服务
 public class Configserver7878Application {
--- codes/gateway8989/src/main/java/com/filters/CustomGlobalFilter.java
+++ codes/gateway8989/src/main/java/com/filters/CustomGlobalFilter.java
@@ -1,4 +1,4 @@
-package com.filter;
+package com.filters;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
